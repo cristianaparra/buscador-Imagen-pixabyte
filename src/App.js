0e3b0e56dca1d6fe153d2aa6c63bb8150b3b1980
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-09 22:19:06 -0300

    terminando proyecto, ocultandfo botones del paginador, agregando efecto scrollIntoView

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
       const imagenesPorPagina = 30;
       const key = '18239276-d6d8b1cc88e822e4cec61a0e3';
-      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
 
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
@@ -28,9 +28,14 @@ function App() {
       // calcular el total de paginas
       const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina)
       guardartotalpaginas(calcularTotalPaginas)
+
+      //mover la pantalla al  home
+
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({behavior: 'smooth'})
     }
     consultarApi()
-  }, [busqueda])
+  }, [busqueda, paginaactual])
 
   //definir pagina anterior
   const paginaAnterior = () => {
@@ -65,22 +70,26 @@ function App() {
         <ListadoImagenes
           imagenes={imagenes}
         />
+        {(paginaactual === 1) ? null : (
+          <button
+            type='button'
+            className='bbtn btn-info mr-1'
+            onClick={paginaAnterior}
+          >
+            &laquo; Anterior
+          </button>
+        )}
 
-        <button
-          type='button'
-          className='bbtn btn-info mr-1'
-          onClick={paginaAnterior}
-        >
-          &laquo; Anterior
-        </button>
+        {(paginaactual === totalpaginas) ? null : (
 
-        <button
-          type='button'
-          className='bbtn btn-info mr-1'
-          onClick={paginaSiguiente}>
+          <button
+            type='button'
+            className='bbtn btn-info mr-1'
+            onClick={paginaSiguiente}>
 
-          Anterior &raquo;
-        </button>
+            Siguiente &raquo;
+          </button>
+        )}
       </div>
     </div>
   );
